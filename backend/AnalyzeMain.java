d5931651c00c8e10d3c4f627f88675472420d81b
clembrival <clemence.brival@gmail.com>
2016-06-07 09:23:20 +0100

    Uploaded the correct version of the java program

--- backend/AnalyzeMain.java
+++ backend/AnalyzeMain.java
@@ -2,14 +2,13 @@
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.bayes.NaiveBayesMultinomialText;
+import weka.classifiers.bayes.net.estimate.BayesNetEstimator;
+import weka.classifiers.rules.ZeroR;
+import weka.classifiers.trees.j48.NBTreeClassifierTree;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
-import weka.filters.unsupervised.attribute.RemoveType;
 import weka.filters.unsupervised.attribute.StringToNominal;
-import weka.filters.unsupervised.attribute.StringToWordVector;
 
 
 public class AnalyzeMain 
@@ -18,25 +17,26 @@ public class AnalyzeMain
 	{
 		try
 		{
-			// Read data from file
-			Instances data = DataSource.read("..//datasets//data.arff");
+			// Loading the data
+			Instances data = DataSource.read("data.arff");
 			
-			// Create a copy of the data set which string attributes 
-			// are converted into word vectors
-			StringToWordVector filter = new StringToWordVector();
+			/* Many algorithms can't handle string attributes (book title)
+			 * so we use a filter to transform the string attribute
+			 * into a nominal one. */
+			StringToNominal filter = new StringToNominal();
+			filter.setAttributeRange("1");
 			filter.setInputFormat(data);
+			
+			// Applying the filter to the data
 			Instances new_data = Filter.useFilter(data, filter);
 			
-			// Set index of attribute to predict
+			// Setting the index of the class to be predicted
 			new_data.setClassIndex(data.numAttributes() - 1);
 			
 			Evaluation eval = new Evaluation(new_data);
 			
-			// Algorithm to be used
-			NaiveBayes bayes = new NaiveBayes();
-			
-			// Usisng cross validation to evaluate the classifier
-			eval.crossValidateModel(bayes, new_data, 20, new Random(1));
+			ZeroR classifier = new ZeroR();
+			eval.crossValidateModel(classifier, new_data, 20, new Random(1));
 			
 			System.out.println(eval.toSummaryString("\nResults\n\n", false));
 		}
